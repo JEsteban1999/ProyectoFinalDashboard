5c0c15ed1a56904318aa17dc7afac6161bd7838b
Juan Esteban <juesnaca99@gmail.com>
2025-05-13 11:45:40 -0500

    Ajustes gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,8 @@
 import multiprocessing
 
 workers = 1
-threads = 2
-timeout = 120
-bind = "0.0.0.0:8080"
+threads = 1
+timeout = 300
+bind = "0.0.0.0:$PORT"
 worker_class = "sync"
-keepalive = 5
+preload_app = True
