a883cae8ef7f1675ee25947241897ad7a9561989
Juan Esteban <juesnaca99@gmail.com>
2025-05-13 17:22:50 -0500

    Cambios app y gunicorn

--- app.py
+++ app.py
@@ -190,5 +190,5 @@ def dashboard():
         return f"Error al generar el dashboard: {str(e)}", 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
 workers = 1
 threads = 1
 timeout = 120
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:10000"
