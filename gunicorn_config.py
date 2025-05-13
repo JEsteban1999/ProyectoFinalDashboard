66f6d1edecf791c0e795735e11ed87fbeee0c918
Juan Esteban <juesnaca99@gmail.com>
2025-05-13 16:15:38 -0500

    Cambios gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,9 @@
 import multiprocessing
+import os
 
 workers = 1
 threads = 1
 timeout = 300
-bind = "0.0.0.0:$PORT"
+bind = f"0.0.0.0:{os.environ.get('PORT', 8080)}"
 worker_class = "sync"
 preload_app = True
